9b3de29eb279ee6e521cc967f73dea120317f8cd
Stephen Argentina <stephena9871@gmail.com>
2018-11-12 03:16:42 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -148,7 +148,6 @@ def player_stats(name)
   "Ben Gordon"=>{number:"8",shoe:"15",points:"33",rebounds:"3",assists:"2",steals:"1",blocks:"1",slam_dunks:"0"},
   "Brendan Haywood"=>{number:"33",shoe:"15",points:"6",rebounds:"12",assists:"12",steals:"22",blocks:"5",slam_dunks:"12"}
     }}}
-  binding.pry
   if game[:home][:players][name]
     return game[:home][:players][name]
   else
