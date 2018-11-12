eff0ae97e06bed598ffa7901c7e4fc5c3a2ceb98
Stephen Argentina <stephena9871@gmail.com>
2018-11-12 02:57:23 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -102,7 +102,7 @@ def team_names
   "Brendan Haywood"=>{number:"33",shoe:"15",points:"6",rebounds:"12",assists:"12",steals:"22",blocks:"5",slam_dunks:"12"}
     }}}
   teams = []
-  game.each {|i| i.each{|team| teams.push(team)}}
+  game.each {|i| i.each{|team, hash| teams.push(team)}}
   binding.pry
   teams
 end
